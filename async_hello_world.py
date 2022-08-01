ca1315797236905e45a668eda2ea54eee0a44ca3
Canthar90 <d.cieslak90@gmail.com>
2022-08-01 22:51:39 +0200

    Added random numbers generator used 3 times at the same time

--- async_hello_world.py
+++ async_hello_world.py
@@ -8,9 +8,19 @@ async def count():
 async def main():
     await asyncio.gather(count(), count(), count())
 
+async def stuff():
+    print("Stuff")
+
+
+async def py35_coro():
+    """Native coroutine, modern syntax"""
+    await stuff()
+
+
 if __name__ == "__main__":
     import time
     s = time.perf_counter()
     asyncio.run(main())
     elapsed = time.perf_counter() - s
     print(f"{__file__} executed in {elapsed:0.2f} seconds.")
+    asyncio.run(py35_coro())
